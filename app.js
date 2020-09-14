4830897a4f89c5bac088f4da65de06800260f452
mnus2019 <tontoas2018@gmail.com>
2020-09-14 11:43:49 -0800

    Express Generator REST API

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
+
 var app = express();
 
 // view engine setup
@@ -19,6 +23,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
