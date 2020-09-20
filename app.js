733a0acd115798c35b93b6a47eca6de597e007a6
mnus2019 <tontoas2018@gmail.com>
2020-09-19 16:30:03 -0800

    Basic Authentication

--- app.js
+++ app.js
@@ -26,7 +26,31 @@ connect.then(() => console.log('Connected correctly to server'),
 
 var app = express();
 
-// view engine setup
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+  }
+
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === 'admin' && pass === 'password') {
+      return next(); // authorized
+  } else {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      return next(err);
+  }
+}
+
+app.use(auth);
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
