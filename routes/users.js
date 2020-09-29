48b36e6876b09535e350a0c4e5ffbdb323cd428a
mnus2019 <tontoas2018@gmail.com>
2020-09-28 17:25:09 -0800

    Passport Facebook

--- routes/users.js
+++ routes/users.js
@@ -56,6 +56,16 @@ router.post('/login',cors.corsWithOptions, passport.authenticate('local'), (req,
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+    if (req.user) {
+        const token = authenticate.getToken({_id: req.user._id});
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: true, token: token, status: 'You are successfully logged in!'});
+    }
+});
+
 router.get('/logout',cors.corsWithOptions, (req, res, next) => {
     if (req.session) {
       req.session.destroy();
